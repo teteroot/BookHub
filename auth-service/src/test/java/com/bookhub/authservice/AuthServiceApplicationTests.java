3c6411f69e1aceb92e57c2d32604265e6f8cc17c
NikolaiUnknown <thestonmix@gmail.com>
2026-04-26 19:52:10 +0300

    initial auth-service

---
+++ auth-service/src/test/java/com/bookhub/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookhub.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
