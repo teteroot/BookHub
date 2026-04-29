1ba0cc5f543552c68974e7fb9dc218c7cfb6de37
NikolaiUnknown <thestonmix@gmail.com>
2026-04-29 21:05:16 +0300

    refactor: remove unnecessary imports; change lifetime

--- auth-service/src/main/java/com/bookhub/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/bookhub/authservice/AuthServiceApplication.java
@@ -1,10 +1,8 @@
 package com.bookhub.authservice;
 
-import com.bookhub.authservice.security.JwtCore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
