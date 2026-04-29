7968e468bb83fc4967d0a9b474b73edcfa0cdf6c
NikolaiUnknown <thestonmix@gmail.com>
2026-04-29 20:57:23 +0300

    feat: add database entity

---
+++ auth-service/init/postgres/tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email varchar(30) NOT NULL UNIQUE,
+    password varchar(30) NOT NULL UNIQUE,
+    role varchar(10) NOT NULL DEFAULT 'READER'
+);
