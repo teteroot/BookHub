326aeeea7559289e484bcae38773c6984f71b637
NikolaiUnknown <thestonmix@gmail.com>
2026-04-29 20:56:55 +0300

    feat: add User auth model

---
+++ auth-service/src/main/java/com/bookhub/authservice/models/User.java
@@ -0,0 +1,29 @@
+package com.bookhub.authservice.models;
+
+import com.bookhub.authservice.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+}
---
+++ auth-service/src/main/java/com/bookhub/authservice/security/UserDetailsImpl.java
@@ -0,0 +1,35 @@
+package com.bookhub.authservice.security;
+
+import com.bookhub.authservice.models.User;
+import lombok.Getter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+    @Getter
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(()-> user.getRole().toString());
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
