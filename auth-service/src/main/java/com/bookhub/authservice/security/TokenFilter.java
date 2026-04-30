fdb6e74aa7e3351088f65e749107c0437d4a4bf9
NikolaiUnknown <thestonmix@gmail.com>
2026-04-30 20:19:32 +0300

    refactor: complete token filter

---
+++ auth-service/src/main/java/com/bookhub/authservice/security/TokenFilter.java
@@ -0,0 +1,40 @@
+package com.bookhub.authservice.security;
+
+import com.bookhub.authservice.services.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class TokenFilter extends OncePerRequestFilter {
+
+    private final JwtCore jwtCore;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith("Bearer ")){
+            doFilter(request,response,filterChain);
+            return;
+        }
+        String jwt = header.substring(7);
+        var claims = jwtCore.claims(jwt);
+        UUID id = claims.get("uuid", UUID.class);
+        var userDetails = new UserDetailsImpl(userService.loadUserByUUID(id));
+        var auth = new UsernamePasswordAuthenticationToken(userDetails, null,
+                                                                userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        doFilter(request,response,filterChain);
+    }
+}
