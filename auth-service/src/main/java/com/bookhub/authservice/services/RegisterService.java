79eb15471df61a550e3aa33d0dfc2db8724b90d8
NikolaiUnknown <thestonmix@gmail.com>
2026-05-02 14:12:40 +0300

    feat: add register service role and its impl; complete register method

--- auth-service/src/main/java/com/bookhub/authservice/services/RegisterService.java
+++ auth-service/src/main/java/com/bookhub/authservice/services/RegisterService.java
@@ -1,5 +1,7 @@
 package com.bookhub.authservice.services;
 
+import com.bookhub.authservice.enums.UserRole;
+
 public interface RegisterService {
-    void register(String email, String password);
+    void register(String email, String password, UserRole role);
 }
---
+++ auth-service/src/main/java/com/bookhub/authservice/services/impls/RegisterServiceImpl.java
@@ -0,0 +1,32 @@
+package com.bookhub.authservice.services.impls;
+
+import com.bookhub.authservice.enums.UserRole;
+import com.bookhub.authservice.exceptions.extensions.EmailIsAlreadyUsedException;
+import com.bookhub.authservice.models.User;
+import com.bookhub.authservice.repositories.UserRepository;
+import com.bookhub.authservice.services.RegisterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+@Service
+@RequiredArgsConstructor
+public class RegisterServiceImpl implements RegisterService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public void register(String email, String password, UserRole role) {
+        if (userRepository.findUserByEmail(email).isPresent()) {
+            throw new EmailIsAlreadyUsedException(email);
+        }
+        var user = User.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .role(role)
+                .build();
+        userRepository.save(user);
+    }
+
+}
