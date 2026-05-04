c2ef1a8728b110c85371a9ed30dcd4f3d3d71b54
NikolaiUnknown <thestonmix@gmail.com>
2026-05-04 22:04:44 +0300

    refactor: change jwt methods signature and create refresh token generation method

--- auth-service/src/main/java/com/bookhub/authservice/services/AuthService.java
+++ auth-service/src/main/java/com/bookhub/authservice/services/AuthService.java
@@ -2,8 +2,12 @@ package com.bookhub.authservice.services;
 
 import com.bookhub.authservice.dtos.requests.PersonDataRequestDto;
 import com.bookhub.authservice.enums.UserRole;
+import org.springframework.security.core.Authentication;
 
 public interface AuthService {
-    String authenticate(String email, String password);
+    Authentication authenticate(String email, String password);
     void registerNewUser(String email, String password, UserRole role, PersonDataRequestDto personDataRequestDto);
+
+    String generateAccessToken(Authentication authentication);
+    String generateRefreshToken(Authentication authentication);
 }
--- auth-service/src/main/java/com/bookhub/authservice/services/impls/AuthServiceImpl.java
+++ auth-service/src/main/java/com/bookhub/authservice/services/impls/AuthServiceImpl.java
@@ -5,11 +5,13 @@ import com.bookhub.authservice.enums.UserRole;
 import com.bookhub.authservice.ports.ProfileProvisioningPort;
 import com.bookhub.authservice.security.JwtCore;
 import com.bookhub.authservice.services.AuthService;
+import com.bookhub.authservice.services.RefreshTokenService;
 import com.bookhub.authservice.services.RegisterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -23,21 +25,33 @@ public class AuthServiceImpl implements AuthService {
     private final RegisterService registerService;
     private final ProfileProvisioningPort profileProvisioningPort;
     private final JwtCore jwtCore;
+    private final RefreshTokenService refreshTokenService;
 
     @Override
-    public String authenticate(String email, String password) {
+    public Authentication authenticate(String email, String password) {
         var auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 email,
                 password
         ));
         SecurityContextHolder.getContext().setAuthentication(auth);
-        return jwtCore.generateToken(auth);
+        return auth;
     }
 
     @Override
     public void registerNewUser(String email, String password, UserRole role, PersonDataRequestDto personDataRequestDto) {
         registerService.register(email,password,role);
         profileProvisioningPort.createPerson(personDataRequestDto);
+    }
+
+    @Override
+    public String generateAccessToken(Authentication authentication) {
+        return jwtCore.generateToken(authentication);
+    }
+
+    @Override
+    public String generateRefreshToken(Authentication authentication) {
+        //TODO check exist token
+        return String.valueOf(refreshTokenService.generateToken(authentication).getToken());
     }
 
 }
