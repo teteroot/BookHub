5462460af78e13c5ab3ed734d6dbf22443e31fa2
NikolaiUnknown <thestonmix@gmail.com>
2026-05-08 23:57:48 +0300

    feat: init profile-service

---
+++ profile-service/src/test/java/com/bookhub/profileservice/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookhub.profileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
