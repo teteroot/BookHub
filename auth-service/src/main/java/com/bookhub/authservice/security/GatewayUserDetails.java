e5665df47a2558873bbc8f6eac2857f7b22ecdeb
NikolaiUnknown <thestonmix@gmail.com>
2026-05-23 16:25:28 +0300

    refactor: change userDetails token based generation to header based generation

---
+++ auth-service/src/main/java/com/bookhub/authservice/security/GatewayUserDetails.java
@@ -0,0 +1,41 @@
+package com.bookhub.authservice.security;
+
+import com.bookhub.authservice.enums.UserRole;
+import lombok.Getter;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public class GatewayUserDetails implements UserDetails {
+    @Getter
+    private final UUID userId;
+
+    private final UserRole role;
+
+    public GatewayUserDetails(UUID userId, UserRole role) {
+        this.userId = userId;
+        this.role = role;
+    }
+
+    @Override
+    @NullMarked
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role::toString);
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return null;
+    }
+
+    @Override
+    @NullMarked
+    public  String getUsername() {
+        return userId.toString();
+    }
+}
