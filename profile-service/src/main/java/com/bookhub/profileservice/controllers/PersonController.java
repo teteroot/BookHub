cf4373c6a679cebf5e240f5111f5d86af7813d22
NikolaiUnknown <thestonmix@gmail.com>
2026-05-23 18:07:15 +0300

    feat: /me endpoint

--- profile-service/src/main/java/com/bookhub/profileservice/controllers/PersonController.java
+++ profile-service/src/main/java/com/bookhub/profileservice/controllers/PersonController.java
@@ -3,16 +3,15 @@ package com.bookhub.profileservice.controllers;
 import com.bookhub.profileservice.dtos.requests.PersonCreateRequestDto;
 import com.bookhub.profileservice.exceptions.extensions.IncorrectRequestDataException;
 import com.bookhub.profileservice.mappers.PersonMapper;
+import com.bookhub.profileservice.security.GatewayUserDetails;
 import com.bookhub.profileservice.services.PersonService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -30,4 +29,10 @@ public class PersonController {
         personService.createPerson(person);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<String> getMyUUID(@AuthenticationPrincipal GatewayUserDetails userDetails){
+        return ResponseEntity.ok(userDetails.getUserId().toString());
+    }
+
 }
