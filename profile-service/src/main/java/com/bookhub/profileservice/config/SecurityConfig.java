f110de70b626a0557bb9affa1278095ddddcc8d1
NikolaiUnknown <thestonmix@gmail.com>
2026-05-23 18:06:38 +0300

    feat: security configuration for persons endpoints

---
+++ profile-service/src/main/java/com/bookhub/profileservice/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.bookhub.profileservice.config;
+
+import com.bookhub.profileservice.security.GatewayVerificationFilter;
+import com.bookhub.profileservice.security.HeaderFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final HeaderFilter headerFilter;
+    private final GatewayVerificationFilter gatewayVerificationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/**","/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/v1/persons/me").fullyAuthenticated()
+                        .requestMatchers("/api/v1/persons/**").permitAll()
+                        .anyRequest().fullyAuthenticated()
+
+                ).addFilterBefore(gatewayVerificationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(headerFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration){
+        return configuration.getAuthenticationManager();
+    }
+
+}
