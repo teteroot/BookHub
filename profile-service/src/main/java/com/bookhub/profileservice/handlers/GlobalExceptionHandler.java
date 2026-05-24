186aba971c9e5f104355e5c17ba8ad065ca5fc5b
NikolaiUnknown <thestonmix@gmail.com>
2026-05-24 13:24:40 +0300

    feat: exception handling for not found exceptions

--- profile-service/src/main/java/com/bookhub/profileservice/handlers/GlobalExceptionHandler.java
+++ profile-service/src/main/java/com/bookhub/profileservice/handlers/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.bookhub.profileservice.handlers;
 
 
 import com.bookhub.profileservice.dtos.responses.ErrorResponseDto;
+import com.bookhub.profileservice.exceptions.NotFoundException;
 import com.bookhub.profileservice.exceptions.extensions.IncorrectRequestDataException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +17,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(IncorrectRequestDataException.class)
     public ResponseEntity<ErrorResponseDto> handleIncorrectRequestDataException(IncorrectRequestDataException e) {
         var errorResponse = new ErrorResponseDto(e.getMessage(), Instant.now(), 400);
-        return ResponseEntity.badRequest().body(errorResponse);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleNotFoundException(NotFoundException e) {
+        var errorResponse = new ErrorResponseDto(e.getMessage(), Instant.now(), 404);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 }
