a02d3d2e4252d86bd2cac0bfd150462a1476e994
NikolaiUnknown <thestonmix@gmail.com>
2026-05-29 18:34:50 +0300

    bugfix: ignore mapper warnings

--- profile-service/src/main/java/com/bookhub/profileservice/mappers/PersonMapper.java
+++ profile-service/src/main/java/com/bookhub/profileservice/mappers/PersonMapper.java
@@ -6,8 +6,9 @@ import com.bookhub.profileservice.dtos.responses.PersonResponseDto;
 import com.bookhub.profileservice.models.Person;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PersonMapper {
     Person toPerson(PersonCreateRequestDto dto);
     Person toPerson(PersonUpdateRequestDto dto);
