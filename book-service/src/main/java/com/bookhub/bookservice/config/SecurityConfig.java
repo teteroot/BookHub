55900ad808cc25016681090b2319ded88aed32e0
NikolaiUnknown <thestonmix@gmail.com>
2026-06-08 14:41:07 +0300

    refactor: change authorize http requests policy

--- book-service/src/main/java/com/bookhub/bookservice/config/SecurityConfig.java
+++ book-service/src/main/java/com/bookhub/bookservice/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v1/books/**").permitAll()
                         .anyRequest().fullyAuthenticated()
 
                 ).addFilterBefore(gatewayVerificationFilter, UsernamePasswordAuthenticationFilter.class)
