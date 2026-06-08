72a890a65b4c3e0cd793f19b5bd6ff72fa7e9c60
NikolaiUnknown <thestonmix@gmail.com>
2026-06-08 14:22:27 +0300

    feat: create global exception handling

---
+++ book-service/src/main/java/com/bookhub/bookservice/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.bookhub.bookservice.handlers;
+
+import com.bookhub.bookservice.dtos.responses.ErrorResponseDto;
+import com.bookhub.bookservice.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var result = e.getBindingResult();
+        StringBuilder errorMessage = new StringBuilder();
+        result.getFieldErrors()
+                .forEach((error) -> {
+                    errorMessage.append(error.getDefaultMessage());
+                    errorMessage.append("; ");
+                });
+        var errorResponse = new ErrorResponseDto(errorMessage.toString(), Instant.now(), 400);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleNotFoundException(NotFoundException e) {
+        var errorResponse = new ErrorResponseDto(e.getMessage(), Instant.now(), 404);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+}
