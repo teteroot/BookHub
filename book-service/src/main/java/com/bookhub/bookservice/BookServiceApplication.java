f96cd3d8baeb4b5e9ddd57aa7d559980191f4442
NikolaiUnknown <thestonmix@gmail.com>
2026-06-23 19:53:53 +0300

    feat: enable configuration properties

--- book-service/src/main/java/com/bookhub/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/bookhub/bookservice/BookServiceApplication.java
@@ -2,8 +2,10 @@ package com.bookhub.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
