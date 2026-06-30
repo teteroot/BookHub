cc83622c3dc68a69f51da7c306c74a4f67f886f0
NikolaiUnknown <thestonmix@gmail.com>
2026-06-30 13:18:39 +0300

    amend! feat: add countOfPages to dto response and download endpoint
    feat: add countOfPages to dto response and download endpoint

--- book-service/src/main/java/com/bookhub/bookservice/services/BookService.java
+++ book-service/src/main/java/com/bookhub/bookservice/services/BookService.java
@@ -1,13 +1,16 @@
 package com.bookhub.bookservice.services;
 
 import com.bookhub.bookservice.models.Book;
+import com.bookhub.bookservice.models.Page;
 
 import java.io.InputStream;
 import java.util.UUID;
 
 public interface BookService {
-
+    InputStream loadBookStream(UUID authorId, UUID bookId);
     Book loadBookByUUID(UUID uuid);
     void createBook(Book book, UUID authorId);
     void createBookContent(UUID bookId, UUID authorId, InputStream content, Long size);
+    Page loadPageByBookIdAndPageNumber(UUID bookId, Integer pageNumber);
+    Integer getCountOfPages(UUID uuid);
 }
