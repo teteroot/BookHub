ea939714082c00cf782e15467f2af27f96c7bbbd
NikolaiUnknown <thestonmix@gmail.com>
2026-07-10 18:09:41 +0300

    feat: create update page endpoint

--- book-service/src/main/java/com/bookhub/bookservice/controllers/PageController.java
+++ book-service/src/main/java/com/bookhub/bookservice/controllers/PageController.java
@@ -2,17 +2,19 @@ package com.bookhub.bookservice.controllers;
 
 import com.bookhub.bookservice.security.GatewayUserDetails;
 import com.bookhub.bookservice.services.BookOrchestrator;
+import com.bookhub.bookservice.validators.PDFValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -21,6 +23,7 @@ import java.util.UUID;
 public class PageController {
 
     private final BookOrchestrator bookOrchestrator;
+    private final PDFValidator pdfValidator;
 
     @GetMapping("/{pageNumber}")
     public ResponseEntity<Resource> getPageByPageNumber(@AuthenticationPrincipal GatewayUserDetails userDetails,
@@ -31,6 +34,20 @@ public class PageController {
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_PDF)
                 .body(new InputStreamResource(pageStream));
+
+    }
+
+    @PatchMapping("/{pageNumber}")
+    @PreAuthorize("hasRole('AUTHOR')")
+    public ResponseEntity<Void> updatePageByPageNumber(@AuthenticationPrincipal GatewayUserDetails userDetails,
+                                                       @PathVariable UUID bookId,
+                                                       @PathVariable Integer pageNumber,
+                                                       @RequestBody MultipartFile pageContent) throws IOException {
+        pdfValidator.validateBookPDF(pageContent);
+        try(InputStream content = pageContent.getInputStream()) {
+            bookOrchestrator.updatePageContent( userDetails.getUserId() ,bookId, pageNumber, content);
+        }
+        return ResponseEntity.ok().build();
 
     }
 }
