282f90f7e8b20c182bf0d3921e889c4e158f2047
NikolaiUnknown <thestonmix@gmail.com>
2026-07-16 10:11:29 +0300

    feat: rewrite base ImageIO api to Thumbnails

--- book-service/src/main/java/com/bookhub/bookservice/services/PDFService.java
+++ book-service/src/main/java/com/bookhub/bookservice/services/PDFService.java
@@ -1,5 +1,7 @@
 package com.bookhub.bookservice.services;
 
+import org.springframework.http.MediaType;
+
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.List;
@@ -8,4 +10,6 @@ public interface PDFService {
 
     List<Path> loadPages(InputStream content);
     Path collectBookFromPages(List<Path> pages);
+
+    Path convertSinglePageToImage(Path pageFile, MediaType type, int dpi);
 }
--- book-service/src/main/java/com/bookhub/bookservice/services/impls/PDFServiceImpl.java
+++ book-service/src/main/java/com/bookhub/bookservice/services/impls/PDFServiceImpl.java
@@ -1,14 +1,19 @@
 package com.bookhub.bookservice.services.impls;
 
+import com.bookhub.bookservice.exceptions.extensions.PDFWorkerException;
 import com.bookhub.bookservice.services.FileTempService;
 import com.bookhub.bookservice.services.PDFService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.io.MemoryUsageSetting;
 import org.apache.pdfbox.io.RandomAccessReadBuffer;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -17,6 +22,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class PDFServiceImpl implements PDFService {
@@ -34,8 +40,9 @@ public class PDFServiceImpl implements PDFService {
                 }
             }
         } catch (IOException e) {
+            log.error("Error while loading pages from PDF", e);
             fileTempService.deleteQuietly(pages);
-            throw new RuntimeException(e);
+            throw new PDFWorkerException();
         }
         return pages;
     }
@@ -50,6 +57,23 @@ public class PDFServiceImpl implements PDFService {
             }
             mergerUtility.mergeDocuments(MemoryUsageSetting.setupMixed(10 * 1024 * 1024).streamCache);
         });
+    }
+
+    @Override
+    public Path convertSinglePageToImage(Path pageFile, MediaType type, int dpi) {
+        try (PDDocument document = Loader.loadPDF(pageFile.toFile())){
+            PDFRenderer renderer = new PDFRenderer(document);
+            var bufferedImage = renderer.renderImageWithDPI(0, dpi);
+            return fileTempService.writeToTempFile("cover-",
+                    ".%s".formatted(type.getSubtype()),
+                    os -> Thumbnails.of(bufferedImage)
+                    .outputFormat(type.getSubtype())
+                    .scale(1)
+                    .toOutputStream(os));
+        } catch (Exception e) {
+            log.error("Error while converting PDF to image", e);
+            throw new PDFWorkerException();
+        }
     }
 
 }
