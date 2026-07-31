d6b38402781ac1cdd20d19475dce527fb93cd4a5
NikolaiUnknown <thestonmix@gmail.com>
2026-07-31 11:44:07 +0000

    refactor: replace secrets and configure RestClient

--- profile-service/src/main/java/com/bookhub/profileservice/security/GatewayVerificationFilter.java
+++ profile-service/src/main/java/com/bookhub/profileservice/security/GatewayVerificationFilter.java
@@ -1,29 +1,30 @@
 package com.bookhub.profileservice.security;
 
+import com.bookhub.profileservice.config.properties.SecurityOriginProperties;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NullMarked;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @Component
 public class GatewayVerificationFilter extends OncePerRequestFilter {
 
-    @Value("${security.origin.gateway.secret}")
-    private String gatewaySecret;
+    private final SecurityOriginProperties securityOriginProperties;
 
-    private final String REQUEST_HEADER_NAME = "X-Gateway-Secret";
+    private static final String REQUEST_HEADER_NAME = "X-Gateway-Secret";
 
     @Override
     @NullMarked
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var secret = request.getHeader(REQUEST_HEADER_NAME);
-        if (secret == null || !secret.equals(gatewaySecret)) {
+        if (secret == null || !secret.equals(securityOriginProperties.getGatewaySecret())) {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             return;
         }
