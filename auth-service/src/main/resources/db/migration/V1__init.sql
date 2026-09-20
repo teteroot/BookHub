c09946b3446d49c2de87e202d59f6d9b6d3b8039
NikolaiUnknown <thestonmix@gmail.com>
2026-09-20 15:50:47 +0300

    bugfix: add cascade delete and change email max length

--- auth-service/src/main/resources/db/migration/V1__init.sql
+++ auth-service/src/main/resources/db/migration/V1__init.sql
@@ -1,11 +1,12 @@
 CREATE TABLE users(
                       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                      email varchar(30) NOT NULL UNIQUE,
-                      password varchar NOT NULL UNIQUE,
+                      email varchar(254) NOT NULL UNIQUE,
+                      password varchar NOT NULL,
                       role varchar(10) NOT NULL DEFAULT 'READER'
 );
 CREATE TABLE refresh_tokens(
                                token UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                               user_id UUID NOT NULL UNIQUE REFERENCES users(id),
+                               user_id UUID NOT NULL UNIQUE REFERENCES users(id)
+                                   ON DELETE CASCADE,
                                expiration TIMESTAMP NOT NULL DEFAULT now()
 );
