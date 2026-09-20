1197707b3633f621f75357e194eedb4e1ace5904
NikolaiUnknown <thestonmix@gmail.com>
2026-09-20 16:11:05 +0300

    refactor: catch adapter errors except 4xx and 5xx

--- auth-service/src/main/java/com/bookhub/authservice/adapters/ProfileRestAdapter.java
+++ auth-service/src/main/java/com/bookhub/authservice/adapters/ProfileRestAdapter.java
@@ -5,9 +5,11 @@ import com.bookhub.authservice.ports.ProfileProvisioningPort;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -23,17 +25,21 @@ public class ProfileRestAdapter extends RestAdapter implements ProfileProvisioni
     private String baseUrl;
     @Override
     public void createPerson(String id, PersonDataRequestDto personData) {
-        restClient.post()
-                .uri("%s/api/v1/persons".formatted(baseUrl))
-                .header(GATEWAY_VERIFICATION_HEADER_NAME,GATEWAY_VERIFICATION_SECRET)
-                .header(USER_ID_HEADER_NAME, id)
-                .header(USER_ROLE_HEADER_NAME, personData.getRole().name())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(personData)
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError,(request, response) -> {
-                    throw new ResponseStatusException(response.getStatusCode(),new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8));
-                })
-                .toBodilessEntity();
+        try {
+            restClient.post()
+                    .uri("%s/api/v1/persons".formatted(baseUrl))
+                    .header(GATEWAY_VERIFICATION_HEADER_NAME,GATEWAY_VERIFICATION_SECRET)
+                    .header(USER_ID_HEADER_NAME, id)
+                    .header(USER_ROLE_HEADER_NAME, personData.getRole().name())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(personData)
+                    .retrieve()
+                    .onStatus(HttpStatusCode::isError,(request, response) -> {
+                        throw new ResponseStatusException(response.getStatusCode(),new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8));
+                    })
+                    .toBodilessEntity();
+        } catch (ResourceAccessException e) {
+            throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Profile service unavailable");
+        }
     }
 }
