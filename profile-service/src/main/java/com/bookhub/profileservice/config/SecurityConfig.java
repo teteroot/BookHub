b46da5e8839da2c8315e4f4c167516473074b6b4
Nikolai Teteruk <thestonmix@gmail.com>
2026-09-22 19:41:08 +0300

    bugfix: fix role prefix in user details; add @EnableMethodSecurity and internal verification filter

--- profile-service/src/main/java/com/bookhub/profileservice/config/SecurityConfig.java
+++ profile-service/src/main/java/com/bookhub/profileservice/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.bookhub.profileservice.config;
 
 import com.bookhub.profileservice.security.filters.GatewayVerificationFilter;
 import com.bookhub.profileservice.security.filters.HeaderFilter;
+import com.bookhub.profileservice.security.filters.InternalVerificationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -18,11 +20,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final HeaderFilter headerFilter;
     private final GatewayVerificationFilter gatewayVerificationFilter;
+    private final InternalVerificationFilter internalVerificationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
@@ -39,8 +43,9 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/persons/**").permitAll()
                         .anyRequest().fullyAuthenticated()
 
-                ).addFilterBefore(gatewayVerificationFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(headerFilter, UsernamePasswordAuthenticationFilter.class);
+                ).addFilterBefore(internalVerificationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(headerFilter, InternalVerificationFilter.class)
+                .addFilterBefore(gatewayVerificationFilter, HeaderFilter.class );
         return http.build();
     }
 
