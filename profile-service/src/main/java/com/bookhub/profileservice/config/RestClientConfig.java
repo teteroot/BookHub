102744d90194e3d6c0131b78141c069472386a58
Nikolai Teteruk <thestonmix@gmail.com>
2026-09-23 18:36:25 +0300

    feat: create connection timeout and read timeout in rest clients

--- profile-service/src/main/java/com/bookhub/profileservice/config/RestClientConfig.java
+++ profile-service/src/main/java/com/bookhub/profileservice/config/RestClientConfig.java
@@ -2,14 +2,23 @@ package com.bookhub.profileservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
 
     @Bean
     public RestClient restClient(){
-        return RestClient.create();
+        var simpleClient = new SimpleClientHttpRequestFactory();
+        simpleClient.setConnectTimeout(Duration.ofSeconds(3));
+        simpleClient.setReadTimeout(Duration.ofSeconds(5));
+        return RestClient.builder()
+                .requestFactory(simpleClient)
+                .build();
+
     }
 
 }
