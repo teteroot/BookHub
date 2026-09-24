819fb6b14ebecc555a8c55fcb8352be6cae7a1ab
Nikolai Teteruk <thestonmix@gmail.com>
2026-09-24 18:24:12 +0300

    bugfix: hide intern exceptions from foreign services

--- profile-service/src/main/java/com/bookhub/profileservice/adapters/BookRestAdapter.java
+++ profile-service/src/main/java/com/bookhub/profileservice/adapters/BookRestAdapter.java
@@ -7,6 +7,7 @@ import com.bookhub.profileservice.exceptions.extensions.RemoteServiceException;
 import com.bookhub.profileservice.ports.BookProvisioningPort;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestClient;
 
 import java.nio.charset.StandardCharsets;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class BookRestAdapter extends RestAdapter implements BookProvisioningPort {
@@ -36,8 +38,12 @@ public class BookRestAdapter extends RestAdapter implements BookProvisioningPort
                     .header(USER_ID_HEADER_NAME, personId)
                     .header(USER_ROLE_HEADER_NAME, UserRole.READER.name())
                     .retrieve()
-                    .onStatus(HttpStatusCode::isError, (request, response) -> {
-                        throw new RemoteServiceException(new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8), response.getStatusCode());
+                    .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                        throw new RemoteServiceException(new String(response.getBody().readNBytes(2048), StandardCharsets.UTF_8), response.getStatusCode());
+                    })
+                    .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+                        log.error("Internal Server Error", response.getBody().readAllBytes());
+                        throw new RemoteInternalServerErrorException();
                     })
                     .toBodilessEntity();
         } catch (ResourceAccessException e) {
@@ -56,8 +62,12 @@ public class BookRestAdapter extends RestAdapter implements BookProvisioningPort
                     .header(USER_ID_HEADER_NAME, personId)
                     .header(USER_ROLE_HEADER_NAME, UserRole.READER.name())
                     .retrieve()
-                    .onStatus(HttpStatusCode::isError, (request, response) -> {
-                        throw new RemoteServiceException(new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8), response.getStatusCode());
+                    .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                        throw new RemoteServiceException(new String(response.getBody().readNBytes(2048), StandardCharsets.UTF_8), response.getStatusCode());
+                    })
+                    .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+                        log.error("Internal Server Error", response.getBody().readAllBytes());
+                        throw new RemoteInternalServerErrorException();
                     })
                     .toBodilessEntity();
         } catch (ResourceAccessException e) {
@@ -76,8 +86,12 @@ public class BookRestAdapter extends RestAdapter implements BookProvisioningPort
                     .header(USER_ID_HEADER_NAME, personId)
                     .header(USER_ROLE_HEADER_NAME, UserRole.READER.name())
                     .retrieve()
-                    .onStatus(HttpStatusCode::isError, (request, response) -> {
-                        throw new RemoteServiceException(new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8), response.getStatusCode());
+                    .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                        throw new RemoteServiceException(new String(response.getBody().readNBytes(2048), StandardCharsets.UTF_8), response.getStatusCode());
+                    })
+                    .onStatus(HttpStatusCode::is5xxServerError, (request, response) -> {
+                        log.error("Internal Server Error", response.getBody().readAllBytes());
+                        throw new RemoteInternalServerErrorException();
                     })
                     .toBodilessEntity();
         } catch (ResourceAccessException e) {
